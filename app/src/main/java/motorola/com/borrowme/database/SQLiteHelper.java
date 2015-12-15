707e5e72bcc0a12abf054c7ff96ab4d5b4f5c7dc
Jonas Oliveira <jonasoli@motorola.com>
2015-12-15 17:04:10 -0200

    Collection entity, dao and updates.

--- app/src/main/java/motorola/com/borrowme/database/SQLiteHelper.java
+++ app/src/main/java/motorola/com/borrowme/database/SQLiteHelper.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import motorola.com.borrowme.database.dao.CollectionDAO;
+
 /**
  * Created by Caio on 15/12/2015.
  */
@@ -12,6 +14,18 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Borrow.db";
+
+    private static SQLiteHelper instance;
+    static private Context c;
+
+    public static SQLiteHelper getInstance(Context context) {
+        if(instance == null){
+            instance = new SQLiteHelper(context);
+            c = context;
+        }
+
+        return instance;
+    }
 
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,9 +35,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         try {
-            db.execSQL("CREATE TABLE COLLECTION ( " +
-                        "ID integer, " +
-                        "NAME string); ");
+            db.execSQL(CollectionDAO.TB_COLLECTION_CREATE);
         }
         catch (SQLiteException e){
 
---
+++ app/src/main/java/motorola/com/borrowme/database/entities/CollectionEntity.java
@@ -0,0 +1,33 @@
+package motorola.com.borrowme.database.entities;
+
+/**
+ * Created by jonasoliveira on 15/12/15.
+ */
+public class CollectionEntity {
+
+    private long _id;
+    private String name;
+
+    public CollectionEntity(long _id, String name) {
+        this._id = _id;
+        this.name = name;
+    }
+
+    public CollectionEntity() {}
+
+    public CollectionEntity(String name) {
+        this.name = name;
+    }
+
+    public long get_id() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
