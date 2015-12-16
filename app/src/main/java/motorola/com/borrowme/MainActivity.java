a51efa0149b5566de5735431d267cee8cc1a9a4a
marcels <marcels@motorola.com>
2015-12-16 13:52:58 -0200

    Layout preliminar para tela de Collections

--- app/src/main/java/motorola/com/borrowme/MainActivity.java
+++ app/src/main/java/motorola/com/borrowme/MainActivity.java
@@ -4,13 +4,54 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.collections);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Collection 1",
+                "Collection 2",
+                "Collection 3",
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // ListView Clicked item index
+                int itemPosition     = position;
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 
     @Override
@@ -28,7 +69,7 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add_collection) {
             return true;
         }
 
