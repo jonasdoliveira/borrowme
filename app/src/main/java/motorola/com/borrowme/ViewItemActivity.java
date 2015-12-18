b93fee748e5a37754d8f2c0abe80be65a9d9ea98
marcels <marcels@motorola.com>
2015-12-18 15:40:09 -0200

    Tela de Visualizar Item quase completa

--- app/src/main/java/motorola/com/borrowme/ViewItemActivity.java
+++ app/src/main/java/motorola/com/borrowme/ViewItemActivity.java
@@ -2,57 +2,98 @@ package motorola.com.borrowme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import motorola.com.borrowme.R;
 import motorola.com.borrowme.database.dao.ItemsDAO;
+import motorola.com.borrowme.database.dao.PersonDAO;
 import motorola.com.borrowme.database.entities.ItemEntity;
+import motorola.com.borrowme.database.entities.PersonEntity;
 
 public class ViewItemActivity extends Activity {
 
-    private Button btnSaveItem;
-    private EditText viewName;
-    private EditText viewDescription;
-    private EditText viewCode;
+    long itemID;
+
+    private Button btnBorrowItem;
+    private Button btnReturnItem;
+    private TextView viewName;
+    private TextView viewDescription;
+    private TextView viewCode;
+    private TextView contentPerson;
+    private TextView contentPhone;
+    private TextView contentEmail;
+    private View whenBorrowed;
+    private View whenNotBorrowed;
 
     private ItemsDAO itemsDAO;
+    private PersonDAO personDAO;
+    private PersonEntity personEntity;
+    private ItemEntity itemEntity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_view_item);
+
 
         itemsDAO = ItemsDAO.getInstance(this);
+        personDAO = PersonDAO.getInstance(this);
 
-        viewName = (EditText) findViewById(R.id.edt_name);
-        viewDescription = (EditText) findViewById(R.id.edt_description);
-        viewCode = (EditText) findViewById(R.id.edt_code);
-        btnSaveItem = (Button) findViewById(R.id.btn_save_item);
 
-        long itemID = getIntent().getLongExtra("ITEM_ID", 0);
+        viewName = (TextView) findViewById(R.id.view_name);
+        viewDescription = (TextView) findViewById(R.id.view_description);
+        viewCode = (TextView) findViewById(R.id.view_code);
+        
+        contentPerson = (TextView) findViewById(R.id.content_person);
+        contentPhone = (TextView) findViewById(R.id.content_phone);
+        contentEmail = (TextView) findViewById(R.id.content_email);
+
+        btnBorrowItem = (Button) findViewById(R.id.btn_borrow_item);
+        btnReturnItem = (Button) findViewById(R.id.btn_return_item);
+
+        whenBorrowed = findViewById(R.id.view_when_borrowed);
+        whenNotBorrowed = findViewById(R.id.view_when_not_borrowed);
+
+        itemID = getIntent().getLongExtra("ITEM_ID", 0);
         if(itemID != 0){
-            ItemEntity itemEntity = itemsDAO.selectById(itemID);
+            itemEntity = itemsDAO.selectById(itemID);
             viewName.setText(itemEntity.getName());
             viewDescription.setText(itemEntity.getDescription());
             viewCode.setText(itemEntity.getCode());
+
+            if(itemEntity.getPersonId() != -1) {
+                personEntity = personDAO.selectById(itemEntity.getPersonId());
+                contentPerson.setText(personEntity.getName());
+                contentPhone.setText(personEntity.getPhone());
+                contentEmail.setText(personEntity.getEmail());
+                whenBorrowed.setVisibility(View.VISIBLE);
+                whenNotBorrowed.setVisibility(View.GONE);
+            } else {
+                whenBorrowed.setVisibility(View.GONE);
+                whenNotBorrowed.setVisibility(View.VISIBLE);
+            }
         }
 
-        btnSaveItem.setOnClickListener(new View.OnClickListener() {
+        btnBorrowItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ItemEntity itemEntity = new ItemEntity(
-                        0000000001, //TODO: GET THIS FROM CONTEXT
-                        viewName.getText().toString(),
-                        viewDescription.getText().toString(),
-                        viewCode.getText().toString(),
-                        -1);
+                //TODO: TRANSITION TO PEOPLE ACTIVITY
+            }
+        });
 
-                itemsDAO.insert(itemEntity);
-                finish();
+        btnReturnItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemEntity.setPersonId(-1);
+                itemsDAO.update(itemEntity);
+                whenBorrowed.setVisibility(View.GONE);
+                whenNotBorrowed.setVisibility(View.VISIBLE);
             }
         });
     }
